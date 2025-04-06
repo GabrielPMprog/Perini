77cd4b773483052a1fc01c128142957ec6b9224b
Gabriel Paiva de Medeiros <gabrielpm.prog@gmail.com>
2025-04-06 18:57:38 -0300

    att

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Perini/', // 👉 coloque aqui o nome do seu repositório
 })
